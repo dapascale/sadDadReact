651bf8852e0da42e3c7ed732c94efeb43225e5d0
Daniel <dapascale@gmail.com>
2020-12-15 20:10:23 -0500

    updates styles and voting structure

--- src/Joke.js
+++ src/Joke.js
@@ -6,18 +6,31 @@ class Joke extends Component {
     constructor(props) {
         super(props);
 
-    }
 
+    }
+    // Get Color Function
+    getColor() {
+        if (this.props.votes >= 15){
+            return "#4caf50";
+        } else if (this.props.votes >= 10) {
+            return "#8bc34a";
+        } else if (this.props.votes >= 6) {
+            return "#ffeb3b";
+        } else if (this.props.votes >= 3) {
+            return "#ff9800";
+        } else {
+            return "#f44336";
+        }
+    }
     render() {
         return(
             <div className="Joke">
                 <div className="jokeButtons">
                     <i className="fa fa-arrow-up" onClick={this.props.upVote}></i>
-                    <span className="votes">{this.props.votes}</span>
+                    <span className="votes" style={{borderColor: this.getColor()}}>{this.props.votes}</span>
                     <i className="fa fa-arrow-down" onClick={this.props.downVote}></i>
                 </div>
                 <p className="jokeText">{this.props.text}</p>
-                <div className="emoji"><i class="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i></div>
             </div>
         )
     }
--- src/JokeList.js
+++ src/JokeList.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import Joke from './Joke';
 import axios from 'axios';
 import './JokeList.css';
+import './Sidebar.css';
 import { v4 as uuidv4 } from 'uuid';
-import Sidebar from './Sidebar';
+import sadDad1 from './assets/mansad1.gif';
+import gladDad1 from './assets/manhappy1.gif';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiceSix } from '@fortawesome/free-solid-svg-icons'
+
+const spinner = <FontAwesomeIcon icon={faDiceSix} />
 
 
 class JokeList extends Component {
@@ -14,41 +20,83 @@ class JokeList extends Component {
 
     // Initialize state
     constructor(props) {
-        super(props)
+        super(props);
         this.state = { 
             // Empty jokes array to be filled with ten jokes at a time
-            jokes: [],
+            jokes: JSON.parse(window.localStorage.getItem("jokes")|| "[]"),
+            loading: false,
         };
+        this.handleClick = this.handleClick.bind(this);
     }
 
     // Make API call
-    async componentDidMount() {
-        // Load Jokes
+    componentDidMount() {
+        // Load Jokes ONLY if there are none
+        if(this.state.jokes.length === 0) {
+            this.getJokes();
+        }
+    }
+
+    // handleClick Function
+    handleClick() {
+        this.setState({
+            loading: true,
+        },
+        this.getJokes
+        )}
+
+    // Get Jokes Function
+    async getJokes() {
         let jokes = [];
-        while(jokes.length < this.props.numOfJokes) {
+        while (jokes.length < this.props.numOfJokes) {
             let res = await axios.get('https://icanhazdadjoke.com/', {
                 headers: {Accept: 'application/json'} 
             });
             jokes.push({id: uuidv4(), text: res.data.joke, votes: 0});
         }
-        this.setState({
-            jokes: jokes,
-        })
+        this.setState(state => ({
+            loading: false,
+            jokes: [...state.jokes, ...jokes]
+        }));
+        window.localStorage.setItem(
+            "jokes",
+            JSON.stringify(jokes)
+        )
     }
 
     // Vote Function
     handleVote = (id, change) => {
-            this.setState(state => ({
-                jokes: state.jokes.map(joke =>
-                    joke.id === id ? {...joke, votes: joke.votes + change} : joke)
-                })
+        this.setState(state => ({
+            jokes: state.jokes.map(joke =>
+                joke.id === id ? {...joke, votes: joke.votes + change} : joke
             )
-        }
+        }),
+        () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+        )
+    }
 
     render() {
+        if (this.state.loading === true) {
+            return (
+                <div className="spinner">
+                    <i className="title2 far fa-8x fa-compact-disc fa-spin"></i>
+                    <h1 className="title2">Loading...</h1>
+                </div>
+            )
+        }
         return (
             <div className="JokeList">
-                <Sidebar />
+                <div className="Sidebar">
+                    <h1><span className="title1">Make Your </span><span className="title2">Sad Dad</span><span className="title3"> Glad</span></h1>
+                    <button className="moreJokes" onClick={this.handleClick}>
+                    <img 
+                    src={sadDad1} alt="face of a very sad man"
+                    onMouseOver={e => e.currentTarget.src = gladDad1} 
+                    onMouseOut={e => e.currentTarget.src = sadDad1} 
+                    />
+                    new jokes
+                    </button>
+                </div>
                 <div className="jokeListJokes">
                     {this.state.jokes.map(joke => (
                         <Joke 
